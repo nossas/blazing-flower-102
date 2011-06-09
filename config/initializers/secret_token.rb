039297c0929073d7f1f7b654249c6378f1da13d1
Ren Provey <ren.provey@gmail.com>
2011-06-09 16:05:55 -0400

    Initial Rails 3.1.rc3 app generation.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MeuRio::Application.config.secret_token = '5c9d8f62e1030c0b3e4867237ef4e937bb4dc2537d43d044ccaeaab52ad06c8fba8e3b1a78623bae5de608107a5f87ed095307d6b56a844afb4b216ecb9c3fe1'
