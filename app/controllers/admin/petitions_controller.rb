f755704ab3896c102ac84972d92043ccd0991a16
Ren Provey <ren.provey@gmail.com>
2011-06-22 12:25:15 -0400

    [#14670483] Added authenticate_user! before filter to Admin::PetitionsController, so the user will be redirected to sign_in if not authenticated.

--- app/controllers/admin/petitions_controller.rb
+++ app/controllers/admin/petitions_controller.rb
@@ -1,6 +1,9 @@
 class Admin::PetitionsController < ApplicationController
+  before_filter :authenticate_user!
+
   load_and_authorize_resource
   inherit_resources
+
   actions :index, :new
   respond_to :json, :only => [:index]
   respond_to :html, :only => [:new]
