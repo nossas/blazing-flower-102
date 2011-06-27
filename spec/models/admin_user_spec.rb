ff3cbe49badac32efc5ab574b74cd44ae85948c0
Ren Provey + Serena Wales <ren.provey+rp+sw@gmail.com>
2011-06-27 12:35:27 -0400

    Adding back AdminUser spec.

---
+++ spec/models/admin_user_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe AdminUser do
+  it { should validate_presence_of :first_name }
+  it { should validate_presence_of :last_name }
+  it { should validate_presence_of :email }
+  it { should validate_presence_of :password }
+end
