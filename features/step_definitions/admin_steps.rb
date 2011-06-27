7d222a4fb6d0eadfbb4974075a7ed59bfd694064
Diogo Biazus <diogob@gmail.com>
2011-06-27 14:11:56 -0300

    replaced admin_path for admin_dashboard_path

--- features/step_definitions/admin_steps.rb
+++ features/step_definitions/admin_steps.rb
@@ -4,7 +4,7 @@ end
 
 Given /^I am logged in to the admin section$/ do
   Factory.create(:admin)
-  visit admin_path
+  visit admin_dashboard_path
   fill_in 'admin_user_email', :with => AdminUser.first.email
   fill_in 'admin_user_password', :with => '123-test'
   click_button 'Login'
