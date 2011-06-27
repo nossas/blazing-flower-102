2fbbd442818f7551f5a79c14954e653eabfeb4dc
Nícolas Iensen <nicolas@quavio.com.br>
2011-06-27 11:58:57 -0300

    Refactoring routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,6 @@
 MeuRio::Application.routes.draw do
-
   ActiveAdmin.routes(self)
-
   devise_for :admin_users, ActiveAdmin::Devise.config
-
-  # devise_for :users
-  match 'user_root' => 'Admin::Dashboard#index'
-
   resources :pages, :controller => 'pages', :only => :show
-
   root :to => "pages#show", :id => "index"
-
-  namespace :admin do
-    get "/" => "dashboard#index"
-    resources :petitions
-    resources :users
-    resources :emails
-  end
 end
