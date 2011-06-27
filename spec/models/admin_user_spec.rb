95857f75f59dad6d6fe528046949cabc2462dc82
Ren Provey + Serena Wales <ren.provey+rp+sw@gmail.com>
2011-06-27 12:34:16 -0400

    Removing unused files due to ActiveAdmin integration.

--- spec/models/admin_user_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe AdminUser do
-  it { should validate_presence_of :first_name }
-  it { should validate_presence_of :last_name }
-  it { should validate_presence_of :email }
-  it { should validate_presence_of :password }
-end
