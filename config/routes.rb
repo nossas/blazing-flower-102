8b0641cb8e03e0b0f594844a4aa3fde08d08c299
Diogo Biazus <diogob@gmail.com>
2011-06-27 17:05:22 -0300

    gave priority for devise routes in order to enable the AdminUser registration paths. Added admin_user_root route to redirect users after they have dited their registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 MeuRio::Application.routes.draw do
-  ActiveAdmin.routes(self)
   devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  match 'admin_user_root' => 'admin/dashboard#index'
+
   resources :pages, :controller => 'pages', :only => :show
   root :to => "pages#show", :id => "index"
 end
