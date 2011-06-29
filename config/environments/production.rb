08340ec476409710f680f0a6089dbf15554da83f
Ren Provey <ren.provey@gmail.com>
2011-06-29 11:16:09 -0400

    [#14772571] Added SMTP settings for Heroku's SendGrid plugin (required on Cedar stack).

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,4 +53,13 @@ MeuRio::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.default_url_options = { :host => "http://www.meurio.org.br" }
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
+  }
 end
