1ae694bd72094126f4e5d1b722f84a6d75d94ada
Ren Provey <ren.provey@gmail.com>
2011-06-29 12:43:04 -0400

    [#14772571] Updated production mail settings in hope of fixing reset password URL added mailer views.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,5 +28,4 @@ MeuRio::Application.configure do
   config.action_mailer.default_url_options = { :host => "localhost:3000" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :host => "localhost", :port => 1025 }
-  
 end
