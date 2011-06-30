6affe036b115fb0cd99892227b8e1bd6fdba6000
Diogo Biazus <diogob@gmail.com>
2011-06-30 15:38:56 -0300

    [#14675353] now we render a 404 when petition does not exist or is in an unviewable state

--- app/controllers/petitions_controller.rb
+++ app/controllers/petitions_controller.rb
@@ -1,6 +1,7 @@
 class PetitionsController < ApplicationController
   def show
     @petition = Petition.where(:custom_path => params[:custom_path]).first
+    return render_404 unless @petition and @petition.state == 'published'
 
     @petition_signature = PetitionSignature.new
     @member = Member.new
