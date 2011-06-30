f17a9459ad804d22db226d292e0ea4e03ddb8187
Serena Wales <serenawr@gmail.com>
2011-06-30 17:20:50 -0400

    [#14708171] Add Facebook metadata to Petition view

--- app/controllers/petitions_controller.rb
+++ app/controllers/petitions_controller.rb
@@ -3,6 +3,7 @@ class PetitionsController < ApplicationController
     @petition = Petition.where(:custom_path => params[:custom_path]).first
     return render_404 unless @petition and @petition.state == 'published'
 
+    @taf = @petition.taf
     @petition_signature = PetitionSignature.new
     @member = Member.new
   end
