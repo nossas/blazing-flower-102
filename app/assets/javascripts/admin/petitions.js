d2d19fd1234c4cf03af5a5e620a34f40ff7ad38e
Ren Provey <ren.provey@gmail.com>
2011-07-06 12:20:23 -0400

    Removing unused Javascript libraries.

--- app/assets/javascripts/admin/petitions.js
+++
@@ -1,8 +0,0 @@
-console.log("Loaded /app/assets/javascripts/admin/petitions.js");
-
-$(function(){
-  window.petitionForm = new FormView({
-    el: $('#petition_form'),
-    model: new Petition()
-  });
-});
