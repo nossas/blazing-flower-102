6fa85526b330bf955a6ddeb40cfc25c4604e34ac
Diogo Biazus + Ren Provey <ren.provey+db+rp@gmail.com>
2011-07-06 13:11:27 -0400

    [#15368969] Removing code to handle a site based member registration.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -6,9 +6,6 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if @member.persisted?
       flash[:notice] = "You signed in as #{@member.name}"
       sign_in_and_redirect @member, :event => :authentication
-    else
-      session["devise.facebook_data"] = env["omniauth.auth"]
-      redirect_to new_member_registration_url
     end
   end
 end
