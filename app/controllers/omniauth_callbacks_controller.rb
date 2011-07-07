5412f359b4f86e46b5cc88d803f41e812a02c794
Diogo Biazus + Ren Provey <ren.provey+db+rp@gmail.com>
2011-07-07 11:26:54 -0400

    [#15368969] WIP ProviderAuthorization, Facebook OAuth integration.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,27 +1,16 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    # You need to implement the method below in your model
-    #
-    # p = ProviderAutorization.find_for_facebook_oauth(auth_data, current_member)
-    # if p
-    #   @member = p.member
-    #   flash = "You're logged in"
-    #   redirect root_url
-    # else
-    #   flash = "Somehting went wrong"
-    #   redirect root_url
-    # end
-    #
-    @member = Member.find_for_facebook_oauth(auth_data, current_member)
-
-    if @member.persisted?
-      flash[:notice] = "You signed in as #{@member.name}"
-      return sign_in_and_redirect @member, :event => :authentication
+    p = ProviderAuthorization.find_for_facebook_oauth(auth_data, current_member)
+    if p.persisted?
+      @member = p.member
+      flash[:notice] = "Welcome #{@member.name}"
+      sign_in_and_redirect @member, :event => :authentication
     else
-      logger.warn(@member.errors.to_yaml)
-      return redirect_to root_path
+      flash[:notice] = "You were unable to login"
+      redirect_to root_url
     end
   end
+
   protected
   def auth_data
     env["omniauth.auth"]
--- spec/controllers/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -8,21 +8,27 @@ describe OmniauthCallbacksController do
     subject{ get :facebook }
     it 'should call find_for_facebook_oauth' do
       controller.stub(:auth_data).and_return(FACEBOOK_VALID_AUTH_DATA) 
-      Member.should_receive(:find_for_facebook_oauth).with(FACEBOOK_VALID_AUTH_DATA, nil).and_return(Factory(:member))
+      ProviderAuthorization.should_receive(:find_for_facebook_oauth).with(FACEBOOK_VALID_AUTH_DATA, nil).and_return(Factory(:provider_authorization))
       subject
     end
     context "with wrong auth data" do
       before{ controller.stub(:auth_data).and_return(FACEBOOK_INVALID_AUTH_DATA) }
       it{ expect{subject}.to_not change{Member.count} }
+      it{ expect{subject}.to_not change{ProviderAuthorization.count} }
       it{ should redirect_to(root_path) }
+      it "should set a flash notice" do
+        subject
+        flash[:notice].should == 'You were unable to login'
+      end
     end
     context "with right auth data" do
       before{ controller.stub(:auth_data).and_return(FACEBOOK_VALID_AUTH_DATA) }
       it{ expect{subject}.to change{Member.count}.by(1) }
+      it{ expect{subject}.to change{ProviderAuthorization.count}.by(1) }
       it{ should redirect_to(root_path) }
       it "should set a flash notice" do
         subject
-        flash[:notice].should == 'You signed in as Diogo Biazus'
+        flash[:notice].should == 'Welcome Diogo Biazus'
       end
     end
   end
