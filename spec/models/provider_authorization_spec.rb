f04dd50bc9bb45e60195070713778ff8111e806e
Nícolas Iensen <nicolas@quavio.com.br>
2011-07-08 15:52:32 -0300

    [#15368969] Added missing test cases for 3th party authentication

--- spec/models/provider_authorization_spec.rb
+++ spec/models/provider_authorization_spec.rb
@@ -14,22 +14,81 @@ describe ProviderAuthorization do
   }
 
   describe ".find_for_facebook_oauth" do
+    subject{ ProviderAuthorization.find_for_facebook_oauth(FACEBOOK_VALID_AUTH_DATA) }
+
     context "when both provider authorization and member do not exist" do
-      subject{ ProviderAuthorization.find_for_facebook_oauth(FACEBOOK_VALID_AUTH_DATA) }
       it{ should be_persisted }
       its(:provider){ should == 'facebook' }
       its(:uid){ should == FACEBOOK_VALID_AUTH_DATA['uid'] }
       its(:member){ subject.email.should == FACEBOOK_VALID_AUTH_DATA['extra']['user_hash']['email'] }
+      it{ expect{subject}.should change{Member.count}.by(1) }
+      it{ expect{subject}.should change{ProviderAuthorization.count}.by(1) }
     end
+
+    context "when only the provider authorization does not exist" do
+      before {@member = Factory(:member, :email => FACEBOOK_VALID_AUTH_DATA['extra']['user_hash']['email'])}
+      it{ should be_persisted }
+      its(:provider){ should == 'facebook' }
+      its(:uid){ should == FACEBOOK_VALID_AUTH_DATA['uid'] }
+      its(:member){ subject.email.should == FACEBOOK_VALID_AUTH_DATA['extra']['user_hash']['email'] }
+      it{ expect{subject}.should_not change{Member.count} }
+      it{ expect{subject}.should change{ProviderAuthorization.count}.by(1) }
+    end
+
+    context "when both provider authorization and member exist" do
+
+      before do
+        @member = Factory(:member, :email => FACEBOOK_VALID_AUTH_DATA['extra']['user_hash']['email'])
+        @provider_authorization = Factory(:provider_authorization, :uid => FACEBOOK_VALID_AUTH_DATA['uid'], :member => @member, :provider => "facebook")
+      end
+
+      it{ should be_persisted }
+      its(:provider){ should == 'facebook' }
+      its(:uid){ should == FACEBOOK_VALID_AUTH_DATA['uid'] }
+      its(:member){ subject.email.should == FACEBOOK_VALID_AUTH_DATA['extra']['user_hash']['email']}
+      it{ expect{subject}.should_not change{Member.count} }
+      it{ expect{subject}.should_not change{ProviderAuthorization.count} }
+    end
+
+
   end
 
   describe ".find_for_google_apps_oauth" do
+    subject{ ProviderAuthorization.find_for_google_apps_oauth(GOOGLE_APP_VALID_AUTH_DATA) }
+
     context "when both provider authorization and member do not exist" do
-      subject{ ProviderAuthorization.find_for_google_apps_oauth(GOOGLE_APP_VALID_AUTH_DATA) }
       it{ should be_persisted }
       its(:provider){ should == 'google_apps' }
       its(:uid){ should == GOOGLE_APP_VALID_AUTH_DATA['uid'] }
       its(:member){ subject.email.should == GOOGLE_APP_VALID_AUTH_DATA['user_info']['email'] }
+      it{ expect{subject}.should change{Member.count}.by(1) }
+      it{ expect{subject}.should change{ProviderAuthorization.count}.by(1) }
     end
+
+    context "when only the provider authorization does not exist" do
+      before {@member = Factory(:member, :email => GOOGLE_APP_VALID_AUTH_DATA['user_info']['email'])}
+      it{ should be_persisted }
+      its(:provider){ should == 'google_apps' }
+      its(:uid){ should == GOOGLE_APP_VALID_AUTH_DATA['uid'] }
+      its(:member){ subject.email.should == GOOGLE_APP_VALID_AUTH_DATA['user_info']['email'] }
+      it{ expect{subject}.should_not change{Member.count} }
+      it{ expect{subject}.should change{ProviderAuthorization.count}.by(1) }
+    end
+
+    context "when both provider authorization and member exist" do
+
+      before do
+        @member = Factory(:member, :email => GOOGLE_APP_VALID_AUTH_DATA['user_info']['email'])
+        @provider_authorization = Factory(:provider_authorization, :uid => GOOGLE_APP_VALID_AUTH_DATA['uid'], :member => @member, :provider => "google_apps")
+      end
+
+      it{ should be_persisted }
+      its(:provider){ should == 'google_apps' }
+      its(:uid){ should == GOOGLE_APP_VALID_AUTH_DATA['uid'] }
+      its(:member){ subject.email.should == GOOGLE_APP_VALID_AUTH_DATA['user_info']['email'] }
+      it{ expect{subject}.should_not change{Member.count} }
+      it{ expect{subject}.should_not change{ProviderAuthorization.count} }
+    end
+
   end
 end
