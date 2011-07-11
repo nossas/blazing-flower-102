cba4ab89c825ef1cd9ed8f4f8a14efe10a50550d
Diogo Biazus <diogob@gmail.com>
2011-07-11 11:53:50 -0300

    putting frontend JS in a separate directory to avoid errors with ActiveAdmin loading.

--- app/assets/javascripts/facebox_setup.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-  $.facebox.settings.closeImage = "/images/closelabel.png"
-  $.facebox.settings.loadingImage = "/images/loading.gif"
-
-  $("[rel=facebox]").facebox();
-});
---
+++ app/assets/javascripts/frontend/facebox_setup.js
@@ -0,0 +1,6 @@
+$(function(){
+  $.facebox.settings.closeImage = "/assets/closelabel.png"
+  $.facebox.settings.loadingImage = "/assets/loading.gif"
+
+  $("[rel=facebox]").facebox();
+});
