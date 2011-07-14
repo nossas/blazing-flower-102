ef7f94eb1d609a9247f51ff62760b775972a1957
Diogo Biazus <diogob@gmail.com>
2011-07-14 13:24:41 -0300

    [#15365257] put autofire_email in the after_create of petition_signature

--- app/models/petition_signature.rb
+++ app/models/petition_signature.rb
@@ -2,4 +2,10 @@ class PetitionSignature < ActiveRecord::Base
   belongs_to :petition
   belongs_to :member
 
+  after_create :send_confirmation
+
+  def send_confirmation
+    PetitionMailer.petition_signature_confirmation(self)
+  end
+
 end
