90d139b520075c1ea523ba05d5eb4e29efc9205f
Serena Wales <serenawr@gmail.com>
2011-07-19 18:24:43 -0400

    [#14748111] Show image on Issue show page

--- app/admin/issues.rb
+++ app/admin/issues.rb
@@ -16,4 +16,48 @@ ActiveAdmin.register Issue do
     end
     f.buttons
   end
+
+  show do
+    div({:class => 'panel'}) do
+      h3 'Issue Details'
+      div({:class => 'panel_contents' }) do
+        div({:class => 'attributes_table issue'  }) do
+          table do
+            tr do
+              th do
+                'Name'
+              end
+              td do 
+                issue.name
+              end
+              tr do 
+                th do
+                  'Excerpt'
+                end
+                td do
+                  issue.excerpt
+                end
+              end
+              tr do
+                th do
+                  'Description'
+                end
+                td do
+                  issue.description
+                end
+              end
+              tr do
+                th do 
+                  'Image'
+                end
+                td do
+                  img({:src => issue.image.url})
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
 end
