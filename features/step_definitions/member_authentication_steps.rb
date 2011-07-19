4de36b77cd19bb173178dcae4a42e944951f598f
Nícolas Iensen <nicolas@quavio.com.br>
2011-07-19 00:13:55 -0300

    Header layout and CSS refactoring

--- features/step_definitions/member_authentication_steps.rb
+++ features/step_definitions/member_authentication_steps.rb
@@ -1,10 +1,2 @@
 Given /^I am not logged in$/ do
 end
-
-Then /^I should see the login link$/ do
-  assert(page.has_css?("a.login"))
-end
-
-Then /^I should see the logout link$/ do
-  assert(page.has_css?("a.logout"))
-end
