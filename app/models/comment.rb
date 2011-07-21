4d62c6bc8a7de55308144bb4732ce53ef15e9b8d
Diogo Biazus <diogob@gmail.com>
2011-07-21 17:15:40 -0300

    [#14744905] Implemented the waiting_moderation scope using the MODERATION_THRESHOLD

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,17 @@
 class Comment < ActiveRecord::Base
+  MODERATION_THRESHOLD = 3
+
   belongs_to :commentable, :polymorphic => true
   belongs_to :member
 
-  has_many :comment_flags
+  has_many :flags, :class_name => 'CommentFlag'
 
   validates_presence_of :content
   validates_presence_of :member_id
   validates_presence_of :commentable_id
   validates_presence_of :commentable_type
 
+  def self.waiting_moderation
+    where(["(SELECT count(*) FROM comment_flags WHERE comment_flags.comment_id = comments.id) >= ?", MODERATION_THRESHOLD])
+  end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,7 +5,7 @@ describe Comment do
   it {should validate_presence_of :member_id}
   it {should validate_presence_of :commentable_id}
   it {should validate_presence_of :commentable_type}
-  
+
   describe "debate comment" do
     before do
       @debate_comment = Factory(:debate_comment)
@@ -13,6 +13,28 @@ describe Comment do
 
     it "should respond to debate methods" do
       @debate_comment.commentable.should == Debate.first
+    end
+  end
+
+  describe ".waiting_moderation" do
+    context "with less flags than MODERATION_THRESHOLD" do
+      before do
+        @debate_comment = Factory(:debate_comment)
+        (Comment::MODERATION_THRESHOLD - 1).times{ @debate_comment.flags.create :member => Factory(:member) }
+        Factory(:debate_comment)
+      end
+      subject{ Comment.waiting_moderation }
+      it{ should be_empty }
+    end
+
+    context "with MODERATION_THRESHOLD flags" do
+      before do
+        @debate_comment = Factory(:debate_comment)
+        Comment::MODERATION_THRESHOLD.times{ @debate_comment.flags.create :member => Factory(:member) }
+        Factory(:debate_comment)
+      end
+      subject{ Comment.waiting_moderation }
+      it{ should == [ @debate_comment ] }
     end
   end
 end
