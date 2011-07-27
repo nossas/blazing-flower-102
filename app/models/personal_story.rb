222094908fba839282c2c265283082783d389b6a
Ren Provey + Serena Wales <ren.provey+rp+sw@gmail.com>
2011-07-27 15:46:36 -0400

    [#14748257] Added model, validations, specs.

--- app/models/personal_story.rb
+++ app/models/personal_story.rb
@@ -1,2 +1,9 @@
 class PersonalStory < ActiveRecord::Base
+  belongs_to :issue
+
+  validates_presence_of :issue
+  validates_presence_of :title
+  validates_presence_of :video_embed_code
+
+  validates_inclusion_of :connected_action, :in => ['PETITION', 'DEBATE'], :allow_nil => true
 end
--- spec/models/personal_story_spec.rb
+++ spec/models/personal_story_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe PersonalStory do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :video_embed_code }
+  it { should validate_presence_of :issue }
+  it { should validate_presence_of :title }
+
+  it { should allow_value('PETITION').for(:connected_action) }
+  it { should allow_value('DEBATE').for(:connected_action) }
+  it { should allow_value(nil).for(:connected_action) }
+
+  it { should_not allow_value('SERENA').for(:connected_action) }
+
 end
