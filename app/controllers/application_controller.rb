3d0d488f484a7995ed78b7df176bb7c108112da9
Serena Wales <serenawr@gmail.com>
2011-07-28 10:34:59 -0400

    Redirect to user's previous page after they've signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+ # Overwriting the sign_out and sign_in redirect path method
+  def after_sign_in_path_for(resource_or_scope)
+    if request.referrer
+      request.referrer
+    else
+      super
+    end
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    if request.referrer
+      request.referrer
+    else
+      super
+    end
+  end
+
   def render_404
     render :template => "errors/error_404", :layout => 'application', :status => 404
   end
