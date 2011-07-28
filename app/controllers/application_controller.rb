91d327d41c72a853baedd95c77a9eed1992658bf
Serena Wales <serenawr@gmail.com>
2011-07-28 10:40:28 -0400

    Refactor login redirection logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,19 +3,11 @@ class ApplicationController < ActionController::Base
 
  # Overwriting the sign_out and sign_in redirect path method
   def after_sign_in_path_for(resource_or_scope)
-    if request.referrer
-      request.referrer
-    else
-      super
-    end
+    request.referrer || super
   end
 
   def after_sign_out_path_for(resource_or_scope)
-    if request.referrer
-      request.referrer
-    else
-      super
-    end
+    request.referrer || super
   end
 
   def render_404
