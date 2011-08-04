f062b8dccae6408131283daa26fc278ab619f71f
Diogo Biazus + Ren Provey <ren.provey+db+rp@gmail.com>
2011-08-04 17:56:16 -0400

    [#14712915] Added Job.active scope.

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,7 @@
 class Job < ActiveRecord::Base
   validates_presence_of :title
   validates_presence_of :description
-  validates_presence_of :active
+  validates_inclusion_of :active, :in => [true,false]
+
+  scope :active, where(:active => true)
 end
--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,7 +1,18 @@
 require 'spec_helper'
 
 describe Job do
-  it { should validate_presence_of :title }
-  it { should validate_presence_of :description }
-  it { should validate_presence_of :active }
+  describe "#valid" do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :description }
+  end
+
+  describe ".active" do
+    it "should only return active jobs" do
+      active_job = Factory(:job)
+      Factory(:job, :active => FALSE)
+
+      Job.all.count.should == 2
+      Job.active.should == [active_job]
+    end
+  end
 end
