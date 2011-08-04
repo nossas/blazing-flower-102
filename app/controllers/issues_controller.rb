7a2dae8f03574bedb59c904b149b86faa11bf8b0
Serena Wales <serenawr@gmail.com>
2011-08-04 18:47:02 -0400

    [#15358595] Initial cut of issue archive page.

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -4,4 +4,29 @@ class IssuesController < ApplicationController
     @issue = Issue.where(:id => params[:id]).first
   end
 
+  def archive
+    @issue = Issue.where(:id => params[:id]).first
+
+    articles_unsorted = @issue.petitions + @issue.debates + @issue.personal_stories
+
+    @articles = articles_unsorted.sort{ |a, b| b.created_at <=> a.created_at }
+    @article_count = @articles.length
+    if !params[:page].nil? && params[:page].to_i > 1
+      @page = params[:page].to_i
+      @articles_start = (@page - 1) * 10
+      article_end = @page * 10
+      @articles = @articles[@articles_start..article_end]
+      if @articles.length == 10
+        @articles_end = @articles_start + 10
+      else
+        @articles_end = @articles_start + @articles.length
+      end
+    else 
+      @page = 1
+      @articles = @articles.first(10)
+      @articles_start = 1
+      @articles_end = @articles.length
+    end
+  end
+
 end
