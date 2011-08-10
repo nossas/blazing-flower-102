a6b87fe770a4ca05bb83ce7c6ea2c57feb9874d6
Ren Provey <ren.provey@gmail.com>
2011-08-10 14:52:51 -0400

    [#16081385] Created ActiveAdmin for FeaturedContentWidget form.

--- app/admin/featured_content_widgets.rb
+++ app/admin/featured_content_widgets.rb
@@ -1,6 +1,23 @@
 ActiveAdmin.register FeaturedContentWidget do
   menu :parent => "Widgets"
 
-  filter :title
   filter :link
+
+  index do
+    column :link
+    column :image_url
+    column "Options" do |w|
+      span link_to 'Show', admin_featured_content_widget_path(w)
+      span link_to 'Edit', edit_admin_featured_content_widget_path(w)
+    end
+  end
+
+  form do |f|
+    f.inputs "Featured Content Widget" do
+      f.input :link, :as => :string
+      f.input :image
+    end
+
+    f.buttons
+  end
 end
