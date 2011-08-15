2e5847b4e026b65e9003f177bbe18f07ed09697f
Serena Wales <serenawr@gmail.com>
2011-08-15 17:51:03 -0400

    [#15370805] Add MeuRioTeam model, and admin setup for managing it.

---
+++ app/admin/meu_rio_team_widgets.rb
@@ -0,0 +1,15 @@
+ActiveAdmin.register MeuRioTeamWidget do
+  menu :parent => "Widgets"
+  filter :html_content
+
+  index do
+    column :html_content
+    column "Options" do |w|
+      span link_to 'Show', admin_meu_rio_team_widget_path(w)
+      span link_to 'Edit', edit_admin_meu_rio_team_widget_path(w)
+    end
+  end
+
+  form :partial => "form"
+  
+end
