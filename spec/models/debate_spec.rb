9c31b741123d5a5b65643429a841700d4c43b81f
Diogo Biazus <diogob@gmail.com>
2011-08-15 21:54:09 -0300

    added distinct_member_count to debate model

--- spec/models/debate_spec.rb
+++ spec/models/debate_spec.rb
@@ -8,4 +8,35 @@ describe Debate do
   it { should validate_presence_of :author_email_side_1 }
   it { should validate_presence_of :author_email_side_2 }
 
+  describe "#distinct_member_count" do
+    let(:debate) { Factory(:debate) }
+    subject{ debate.distinct_member_count }
+
+    context "when we have no comments" do
+      it { should == 0 }
+    end
+
+    context "when we have two comments from different members" do
+      before do
+        2.times{ Factory(:debate_comment, :commentable => debate) }
+      end
+      it { should == 2 }
+    end
+
+    context "when we have two comments from the same member" do
+      before do
+        @member = Factory(:provider_authorization).member
+        2.times{ Factory(:debate_comment, :commentable => debate, :member => @member) }
+      end
+      it { should == 1 }
+    end
+
+    context "when we have one comment" do
+      before do
+        Factory(:debate_comment, :commentable => debate)
+      end
+      it { should == 1 }
+    end
+  end
+
 end
