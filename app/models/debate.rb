700169a799a1f512fb73cec1c7d47c1d11577d1b
Serena Wales <serenawr@gmail.com>
2011-08-18 14:38:47 -0400

    Validate for issue on Debate and Petition

--- app/models/debate.rb
+++ app/models/debate.rb
@@ -6,12 +6,15 @@ class Debate < ActiveRecord::Base
   has_many :comments, :as => :commentable
   has_many :members, :through => :comments
 
+  validates_presence_of :issue
+
   validate :authors_are_members
   validates_presence_of :title
   validates_presence_of :question
   validates_presence_of :comment_prompt
   validates_presence_of :author_email_side_1
   validates_presence_of :author_email_side_2
+
 
   def authors_are_members
     errors.add(:author_email_side_1, "Author must be an existing member of Meu Rio") if Member.where(:email => self.author_email_side_1).empty?
