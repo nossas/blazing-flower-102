0797c53654d37894dd1f33f01e1d124b1f2cd36d
Ren Provey <ren.provey@gmail.com>
2011-08-23 14:30:06 -0400

    [#15370919] Slowing down the autoscroll.

--- app/assets/javascripts/frontend/widgets.js
+++ app/assets/javascripts/frontend/widgets.js
@@ -26,7 +26,7 @@ MR.widgets = {
     if(this.find('.scrollable .item').length < 4){
       this.find('.controls').hide();
     } else {
-      this.find('.scrollable').autoscroll({autoplay: true, autopause: true});
+      this.find('.scrollable').autoscroll({interval: 5000, autoplay: true, autopause: true});
     }
     return this.find('.scrollable').scrollable();
   }
