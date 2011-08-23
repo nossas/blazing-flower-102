aff117dbcd3b0d65d5eee88d4bb16fbc7108033a
Ren Provey <ren.provey@gmail.com>
2011-08-23 14:46:24 -0400

    [#16840537] Renamed scroll icons for generic use, created basic Public hearings Widget.

--- app/admin/public_hearings_widget.rb
+++ app/admin/public_hearings_widget.rb
@@ -3,7 +3,9 @@ ActiveAdmin.register PublicHearingsWidget do
   filter :html_content
 
   index do
-    column :html_content
+    column :html_content do |w|
+      raw(w.html_content)
+    end
     column "Options" do |w|
       span link_to 'Show', admin_public_hearings_widget_path(w)
       span link_to 'Edit', edit_admin_public_hearings_widget_path(w)
