bd0613bc6db033465c1dd1ed31ff746848f8cef9
Nícolas Iensen <nicolas@quavio.com.br>
2011-08-30 17:13:43 -0300

    [#15371011] Adding meu rio is answer on the admin memebers list

--- app/admin/members.rb
+++ app/admin/members.rb
@@ -8,10 +8,13 @@ ActiveAdmin.register Member do
     column :first_name
     column :last_name
     column :email
-    column :zona
+    column :meu_rio_is
+    column :created_at do |m|
+      l m.created_at, :format => :short
+    end
     column "Options" do |o|
-      span link_to "Show", admin_issue_path(o)
-      span link_to "Edit", edit_admin_issue_path(o)
+      span link_to "Show", admin_member_path(o)
+      span link_to "Edit", edit_admin_member_path(o)
     end
   end
 end
