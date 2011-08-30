99c9265d09287baac90b38d1fdeaaaef1047f9b2
Ren Provey <ren.provey@gmail.com>
2011-08-30 17:33:02 -0400

    Adding test coverage.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,11 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'simplecov'
+SimpleCov.start 'rails'
+
+SimpleCov.start do
+  add_filter "/app/admin/"
+end
+
+#This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
