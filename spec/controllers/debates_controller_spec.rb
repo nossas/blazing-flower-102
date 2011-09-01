09b36a054a368cc15cefb912670cf4e3f4594098
Diogo Biazus <diogob@gmail.com>
2011-09-01 18:58:09 -0300

    [fix #17032355] now we can click in the comments ins user profile and go directly to the comment

--- spec/controllers/debates_controller_spec.rb
+++ spec/controllers/debates_controller_spec.rb
@@ -2,15 +2,33 @@ require 'spec_helper'
 
 describe DebatesController do
   describe "GET show" do
+    let(:comments) { mock_model(Comment) }
     before do
       debate = mock_model(Debate)
-      debate.stub_chain(:comments, :visible).and_return([mock_model(Comment)])
+      debate.stub_chain(:comments, :visible).and_return(comments)
       debate.stub(:distinct_member_count).and_return(1)
       Debate.stub_chain(:where, :first).and_return(debate)
     end
 
+    context "when we ask for all comments" do
+      before do
+        comments.should_receive(:all).and_return(comments)
+        get :show, :id => 1, :todos => 1
+      end
+      it { should assign_to(:initial_comments) }
+    end
+
+    context "when we do not ask for all comments" do
+      before do
+        comments.should_receive(:first).with(5).and_return(comments)
+        get :show, :id => 1
+      end
+      it { should assign_to(:initial_comments) }
+    end
+
     context "user is logged in" do
       before do
+        comments.should_receive(:first).with(5).and_return(comments)
         controller.stub!(:current_member).and_return(mock_model(Member))
         get :show, :id => 1
       end
@@ -20,6 +38,7 @@ describe DebatesController do
     
     context "user is not logged in" do
       before do
+        comments.should_receive(:first).with(5).and_return(comments)
         get :show, :id => 1
       end
       it { should_not assign_to(:comment) }
