6f22baf94367b086bd7f021137765c600ade079d
Ren Provey <ren.provey@gmail.com>
2011-09-01 17:04:12 -0400

    [#17669607] Standardized handling of HTML Content for respective widgets.

--- app/admin/meu_rio_team_widgets.rb
+++ app/admin/meu_rio_team_widgets.rb
@@ -3,7 +3,9 @@ ActiveAdmin.register MeuRioTeamWidget do
   filter :html_content
 
   index do
-    column :html_content
+    column "HTML Content" do |w|
+      raw w.html_content
+    end
     column "Options" do |w|
       span link_to 'Show', admin_meu_rio_team_widget_path(w)
       span link_to 'Edit', edit_admin_meu_rio_team_widget_path(w)
