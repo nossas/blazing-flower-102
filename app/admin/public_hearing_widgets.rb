8a3d70b3a89d0c5872f40fd561124ef5e5cdc2af
Ren Provey + Serena Wales <ren.provey+rp+sw@gmail.com>
2011-09-08 15:18:19 -0400

    [#17712291] Updated Admin::PublicHearingWidget#show view.

--- app/admin/public_hearing_widgets.rb
+++ app/admin/public_hearing_widgets.rb
@@ -14,4 +14,28 @@ ActiveAdmin.register PublicHearingWidget do
   end
 
   form :partial => "form"
+  
+  show do |w|
+    div({:class => 'panel' }) do
+      h3 'Public Hearing Widget Details'
+      div({:class => 'panel_contents' }) do
+        div({:class => 'attributes_table meu_rio_is_widget' }) do
+          table do
+            tr do
+              th { 'HTML Content' }
+              td { raw w.html_content }
+            end
+            tr do
+              th { 'Created at' }
+              td { l w.created_at, :format => :short }
+            end
+            tr do
+              th { 'Updated at' }
+              td { l w.updated_at, :format => :short }
+            end
+          end
+        end
+      end
+    end
+  end
 end
