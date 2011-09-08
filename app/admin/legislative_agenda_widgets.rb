de7dce73c141a6f6f2bb925173c8cf2f3f904c61
Ren Provey <ren.provey@gmail.com>
2011-09-08 14:40:39 -0400

    [#17712291] Updated Admin::LegislativeAgendaWidget#show view.

--- app/admin/legislative_agenda_widgets.rb
+++ app/admin/legislative_agenda_widgets.rb
@@ -14,4 +14,32 @@ ActiveAdmin.register LegislativeAgendaWidget do
   end
 
   form :partial => "form"
+  
+  show do |w|
+    div({:class => 'panel'}) do
+      h3 'Legislative Agenda Widget Details'
+      div({:class => 'panel_contents' }) do
+        div({:class => 'attributes_table issue'  }) do
+          table do
+            tr do
+              th do 
+                'HTML Content'
+              end
+              td do
+                raw(w.html_content)
+              end
+            end
+            tr do
+              th { 'Created at' }
+              td { l w.created_at, :format => :short }
+            end
+            tr do
+              th { 'Updated at' }
+              td { l w.updated_at, :format => :short }
+            end
+          end
+        end
+      end
+    end
+  end
 end
