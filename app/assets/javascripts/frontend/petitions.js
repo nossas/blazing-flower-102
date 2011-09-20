1abbcd5905037f11481d66414707e147cd8644e7
Ren Provey <ren.provey@gmail.com>
2011-09-20 13:11:38 -0400

    [Fixed #18330205] Updated ajax submission and handling of signature form to be IE8 compatible.

--- app/assets/javascripts/frontend/petitions.js
+++ app/assets/javascripts/frontend/petitions.js
@@ -8,6 +8,8 @@ MR.petitions = {
     //set listeners
     $(window).bind('hashchange', MR.petitions.loadTAF);
     $(window).bind('hashchange', MR.petitions.loadDonation);
+    $('#member_email').change(MR.petitions.check_for_existing_signature_by_email);
+    $('#new_petition_signature').submit(MR.petitions.handle_signature_submit);
 
     $("form#new_petition_signature").validate({
       messages: {
@@ -18,44 +20,47 @@ MR.petitions = {
       }
     });
 
-    $("#member_email").change(function(){
-      $('label.error.already_signed[for="member_email"]').hide();
-      $('form input[type=submit]').removeClass('disabled').removeAttr('disabled');
-      $.getJSON('/petition_signatures', {email: $(this).val(), petition_id: $('form').data('petition_id')})
-      .success(function(data){
-        if(data){
-          $('form input[type=submit]').addClass('disabled').attr('disabled', 'disabled');
-          $('form input#member_email').addClass('error');
-          $('label.error.already_signed[for="member_email"]').css('display', 'block');
-        }
-      });
-    });
-
     $("#petition_signature_comment").keyup(function(){
       $("#comment_tip").html((140 - $("#petition_signature_comment").val().length) + " caracteres");
     });
+  },
 
-    $('.take_action').bind('ajax:success', function(event, data){
-      var $counter = $('span.counter'), $progress = $('.progress'), $goal = $('.goal');
-      // Here we update the counter (if we have one) and the progress (if we have it)
-      if($counter.length != 0){
-        var count = parseInt($counter.html());
-        $counter.html(count + 1);
-        if($goal.length != 0){
-          var goal = parseInt(/(\d+)/.exec($goal.html())[1]);
-          var percent = Math.floor((count * 100) / goal);
-          if(goal && goal > 0 && percent <= 100){
-            $progress.animate({width: percent + '%'});
-          }
+  handle_signature_submit: function(e){
+    e.preventDefault();
+    if($("form#new_petition_signature").valid()){
+      $("#loader").show();
+      $('#submit_btn').hide();
+      var data = $(this).serialize();
+      $.post($(this).attr('action'), data, MR.petitions.handle_signature_response);
+    }
+  },
+
+  handle_signature_response: function(data){
+    var $counter = $('span.counter'), $progress = $('.progress'), $goal = $('.goal');
+    // Here we update the counter (if we have one) and the progress (if we have it)
+    if($counter.length != 0){
+      var count = parseInt($counter.html());
+      $counter.html(count + 1);
+      if($goal.length != 0){
+        var goal = parseInt(/(\d+)/.exec($goal.html())[1]);
+        var percent = Math.floor((count * 100) / goal);
+        if(goal && goal > 0 && percent <= 100){
+          $progress.animate({width: percent + '%'});
         }
       }
-      window.location.hash = data.hash;
-    });
+    }
+    window.location.hash = data.hash;
+  },
 
-    $('#submit_btn').click(function(){
-      if($("form#new_petition_signature").valid()){
-        $("#loader").show();
-        $('#submit_btn').hide();
+  check_for_existing_signature_by_email: function(){
+    $('label.error.already_signed[for="member_email"]').hide();
+    $('form input[type=submit]').removeClass('disabled').removeAttr('disabled');
+    $.getJSON('/petition_signatures', {email: $(this).val(), petition_id: $('form').data('petition_id')})
+    .success(function(data){
+      if(data){
+        $('form input[type=submit]').addClass('disabled').attr('disabled', 'disabled');
+        $('form input#member_email').addClass('error');
+        $('label.error.already_signed[for="member_email"]').css('display', 'block');
       }
     });
   },
