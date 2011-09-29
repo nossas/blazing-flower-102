5afcab2a308d1e917797fb6cf1f4a7c17264ee95
Ren Provey <ren.provey@gmail.com>
2011-09-29 14:11:23 -0400

    [Fixes #19036915] Including archived petitions on Issue#archived page.

--- spec/controllers/issues_controller_spec.rb
+++ spec/controllers/issues_controller_spec.rb
@@ -11,4 +11,27 @@ describe IssuesController do
       response.code.should eq("200")
     end
   end
+
+  describe "GET archive" do
+    it "has a 200 status code" do
+      get :show, :id => @issue.id
+      response.code.should eq("200")
+    end
+
+    it "assigns @articles without any nil items when only published petitions exist" do
+      @published_petition = Factory(:complete_petition, :issue => @issue).tap{ |p| p.publish }
+
+      xhr :get, :archive, :id => @issue.id
+      assigns(:articles).should == [@published_petition]
+    end
+
+    it "assigns @articles with only published and archived petitions" do
+      @draft_petition = Factory(:petition, :issue => @issue)
+      @archived_petition  = Factory(:complete_petition, :issue => @issue).tap{ |p| p.publish && p.archive }
+      @published_petition = Factory(:complete_petition, :issue => @issue).tap{ |p| p.publish }
+
+      xhr :get, :archive, :id => @issue.id, :format => :js
+      assigns(:articles).should == [@published_petition, @archived_petition]
+    end
+  end
 end
