f7bc202d9cc0a40e2490f71b8a99e942decfa2bb
Serena Wales <serenawr@gmail.com>
2011-09-30 12:24:39 -0400

    More testing on member registration controller

--- spec/controllers/member_registration_controller_spec.rb
+++ spec/controllers/member_registration_controller_spec.rb
@@ -24,5 +24,38 @@ describe MemberRegistrationController do
       end
       
     end
+
+    context "member who has already signed a petition" do
+      before do
+        @member = Factory.create(:petition_signature).member
+
+        post :create, {:member => {:first_name => "#{@member.first_name}", :last_name => "#{@member.last_name}", :email => "#{@member.email}", :password => "pw", :password_confirmation => "pw"} }
+      end
+
+      it "should not create a new member" do
+        Member.count.should == 1
+      end
+
+      it "should set the correct flags" do
+        @member = Member.first
+        @member.has_login.should == true
+        @member.has_non_oauth_login.should == true
+      end
+    end
+
+    context "member who has already signed through a third party service" do
+      before do
+        @member = Factory.create(:provider_authorization).member
+        post :create, {:member => {:first_name => "#{@member.first_name}", :last_name => "#{@member.last_name}", :email => "#{@member.email}", :password => "pw", :password_confirmation => "pw"} }
+      end
+
+      it "should not create a new member" do
+        Member.count.should == 1
+      end
+
+      it "should not reset the has_non_oauth_login flag" do
+        Member.first.has_non_oauth_login.should == false
+      end
+    end
   end
 end
