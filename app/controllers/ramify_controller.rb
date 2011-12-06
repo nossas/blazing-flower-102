04cff37e31f8de01156fd2c9d259aaa37e4e8101
Diogo Biazus <diogob@gmail.com>
2011-12-06 00:09:35 -0200

    fixing SSL request in ramify#create_session

--- app/controllers/ramify_controller.rb
+++ app/controllers/ramify_controller.rb
@@ -1,7 +1,12 @@
 class RamifyController < ApplicationController
   def create_session
     uri = URI(SITE['ramify_auth_address'])
-    res = Net::HTTP.post_form(uri, 'uid' => current_member.id, 'name' => current_member.name, 'api_secret' => SITE['ramify_key'])
+    res = nil
+    Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+      req = Net::HTTP::Post.new uri.request_uri
+      req.set_form_data 'uid' => current_member.id, 'name' => current_member.name, 'api_secret' => SITE['ramify_key']
+      res = http.request req
+    end
     render :json => res.body
   end
 
@@ -9,8 +14,8 @@ class RamifyController < ApplicationController
     uri = URI(SITE['ramify_logout_address'])
     res = nil
     Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
-      request = Net::HTTP::Post.new uri.request_uri
-      res = http.request request
+      req = Net::HTTP::Post.new uri.request_uri
+      res = http.request req
     end
     render :json => res.body
   end
