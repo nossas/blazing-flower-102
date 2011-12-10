a0aa02f0da1114fc7c45e25ac71031900915273d
Diogo Biazus <diogob@gmail.com>
2011-12-10 18:07:33 -0200

    just adding some baseic functionality to a generic view to be extended in other MR views

--- app/assets/javascripts/frontend/base_view.js
+++ app/assets/javascripts/frontend/base_view.js
@@ -2,5 +2,9 @@ MR.BaseView = Backbone.View.extend({
   el: 'body',
   isLoggedIn: function(){
     return this.$('#member_panel .flyout').length > 0;
+  },
+
+  loginDialog: function(){
+    $.facebox({div: '#login'});
   }
 });
--- spec/javascripts/base_view_spec.js
+++ spec/javascripts/base_view_spec.js
@@ -1,11 +1,25 @@
 describe("BaseView", function(){
   var view = null;
   beforeEach(function(){
-    view = new MR.BaseView();
+    var body = $('body').append('<div id="member_panel">');
+    view = new MR.BaseView({el: body});
+  });
+
+  describe("#loginDialog", function(){
+    it("should call facebox with #login div", function(){
+      spyOn($, "facebox");
+      view.loginDialog();
+      expect($.facebox).toHaveBeenCalledWith({div: '#login'});
+    });
   });
 
   describe("#isLoggedIn", function(){
+    it("should return true when user is NOT logged in", function(){
+      expect(view.isLoggedIn()).toEqual(false);
+    });
+
     it("should return true when user is logged in", function(){
+      view.$('#member_panel').append('<div class="flyout">');
       expect(view.isLoggedIn()).toEqual(true);
     });
   });
