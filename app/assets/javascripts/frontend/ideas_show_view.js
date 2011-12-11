cf3239ac099cc5fc9ea63db7d6b1b82b9f57abd6
Luiz Fonseca <runeroniek@gmail.com>
2011-12-11 14:13:17 -0300

    Removed not-yet-useful function

--- app/assets/javascripts/frontend/ideas_show_view.js
+++ app/assets/javascripts/frontend/ideas_show_view.js
@@ -1,7 +1,6 @@
 MR.IdeasShowView = MR.BaseView.extend({
   initialize: function(){
     _.bindAll(this, 'fork');
-    _.bindAll(this, 'selectItem');
     this.bindRoutes();
     this.loadLastFragment();
   },
@@ -15,13 +14,5 @@ MR.IdeasShowView = MR.BaseView.extend({
       $.facebox({div: '#confirm_fork_idea'});
     }
   },
-
-  selectItem: function(){
-    this.selectedItem = this.$('.menu a[href=#' + name + ']');
-    this.$('.menu .selected').removeClass('selected');
-    this.selectedItem.addClass('selected');
-    this.$('.content > div').hide();
-    this.$('.content .' + name).show();
-  }
 
 });
