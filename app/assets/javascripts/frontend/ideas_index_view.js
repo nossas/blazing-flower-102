f97a0a52de33a7534ec6e894efd384653e6478dc
Barbara Wolff Dick <barbara@quavio.com.br>
2011-12-12 17:23:33 -0200

    new idea dropdown on style

--- app/assets/javascripts/frontend/ideas_index_view.js
+++ app/assets/javascripts/frontend/ideas_index_view.js
@@ -1,12 +1,21 @@
 MR.IdeasIndexView = MR.BaseView.extend({
   initialize: function(){
-    _.bindAll(this, 'newIdea');
+    var that = this;
+    _.bindAll(this, 'newIdea', 'selectCategory');
     this.bindRoutes();
     this.loadLastFragment();
+    $(document).bind('reveal.facebox', function(){
+      MR.common.setUpDropDowns(that.selectCategory);
+    });
   },
 
   bindRoutes: function(){
     MR.router.bind('route:new_idea', this.newIdea)
+  },
+
+  selectCategory: function(params){
+    $('option', '#facebox #idea_idea_category_id').removeAttr('selected');
+    $('option[value=' + $(params).attr('href') + ']', '#facebox #idea_idea_category_id').attr('selected', 'selected');
   },
 
   newIdea: function(){
