4a32673a24e5fee2592b2b15cd8f3e57f38450f9
Diogo Biazus <diogob@gmail.com>
2011-12-12 21:22:07 -0200

    creating cucumber tests for creating and idea and movinf ideas steps to one file

--- features/step_definitions/ideas_steps.rb
+++ features/step_definitions/ideas_steps.rb
@@ -6,6 +6,14 @@ Given /^(\d+) ideas exist$/ do |count|
   end
 end
 
+Given /^I have (\d+) idea category$/ do |count|
+  @categories ||= []
+  count.to_i.times do
+    f = Factory.create(:idea_category)
+    @categories << f
+  end
+end
+
 Then /^I should see the idea title$/ do
   page.should have_content("#{@ideas.first.title}")
 end
@@ -24,4 +32,16 @@ end
 
 Then /^I should see the new idea dialog$/ do
   page.evaluate_script("$('#facebox form[name=new_idea]:visible').length").should == 1
+end
+
+When /^I fill in my idea information$/ do
+  page.execute_script("$('#facebox input#idea_title').val('test idea')");
+  page.execute_script("$('#facebox textarea#idea_headline').val('headline for the test idea')");
+  page.execute_script("$('option[value=#{@categories.first.id}]', '#facebox select#idea_idea_category_id').attr('selected', 'selected')");
+  page.execute_script("$('#facebox form').submit()");
+  sleep 2
+end
+
+Then /^My idea should be created for that issue$/ do
+  Idea.find_by_title('test idea').should_not be_nil
 end
