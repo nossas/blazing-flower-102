2ff53c9ec8cd75acc7fef96766b52c2824ac1a05
Luiz Fonseca <runeroniek@gmail.com>
2011-12-13 17:12:31 -0300

    Added routes/implementation for #remove idea action

--- app/assets/javascripts/frontend/router.js
+++ app/assets/javascripts/frontend/router.js
@@ -1,8 +1,11 @@
 MR.Router = Backbone.Router.extend({
   routes: {
     'fork': 'fork',
-    'new_idea': 'new_idea'
+    'new_idea': 'new_idea',
+    'remove': 'remove'
   },
   fork: function(){},
-  new_idea: function(){}
+  new_idea: function(){},
+  remove: function(){}
+
 });
