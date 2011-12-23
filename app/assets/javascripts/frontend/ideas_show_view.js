c498aef87ed47a8b6a989f309a2de975b8f50371
Diogo Biazus <diogob@gmail.com>
2011-12-23 11:02:17 -0200

    made .selected class work for tabs in ideas show page

--- app/assets/javascripts/frontend/ideas_show_view.js
+++ app/assets/javascripts/frontend/ideas_show_view.js
@@ -33,15 +33,15 @@ MR.IdeasShowView = MR.EditableView.extend({
   },
 
   versions: function(){
-    this.$('a[href="#versions"]').addClass('selected')
-    this.$('a[href="#description"]').removeClass('selected')
+    this.$('a[href="#versions"] h2').addClass('selected')
+    this.$('a[href="#description"] h2').removeClass('selected')
     this.$('#idea_description').hide();
     this.$('#idea_versions').show();
   },
 
   description: function(){
-    this.$('a[href="#versions"]').removeClass('selected')
-    this.$('a[href="#description"]').addClass('selected')
+    this.$('a[href="#versions"] h2').removeClass('selected')
+    this.$('a[href="#description"] h2').addClass('selected')
     this.$('#idea_description').show();
     this.$('#idea_versions').hide();
   },
