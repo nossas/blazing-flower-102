25e59b14cf2d7875a1289b3ab470e15443cc32fc
Diogo Biazus <diogob@gmail.com>
2012-02-02 17:12:50 -0200

    testing view published ideas

--- features/step_definitions/view_ideas_steps.rb
+++ features/step_definitions/view_ideas_steps.rb
@@ -6,9 +6,17 @@ Given /^(\d+) issue exist$/ do |count|
   end
 end
 
+And /^(\d+) unpublished ideas exists for that issue$/ do |count|
+  @idea ||= []
+  count.to_i.times do |f|
+    f = Factory.create(:idea, :title => 'unpublished idea', :published => false, :issue => @issue.first)
+  end
+end
+
 And /^(\d+) ideas exists for that issue$/ do |count|
   @idea ||= []
   count.to_i.times do |f|
-    f = Factory.create(:idea, :issue => @issue.first)
+    f = Factory.create(:idea, :published => true, :issue => @issue.first)
   end
 end
+
