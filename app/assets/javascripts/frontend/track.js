5ae623b7e4398a71ecf73c703a05ac1f6e626894
Nícolas Iensen <nicolas@quavio.com.br>
2012-02-08 12:03:39 -0200

    Fix track code

--- app/assets/javascripts/frontend/track.js
+++ app/assets/javascripts/frontend/track.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-  $("#new_petition_signature_with_facebook"). submit(function(){ _gaq.push(['_trackEvent', 'Assinatura', "Assinar Via Facebook"]); });
-  $(".share_with_friends form").              submit(function(){ _gaq.push(['_trackEvent', 'Share', "Postar no Mural do Amigo"]); });
-  $("#services .facebook_btn").               submit(function(){ _gaq.push(['_trackEvent', 'Login', "Facebook"]); });
-  $("#services .orkut_btn").                  submit(function(){ _gaq.push(['_trackEvent', 'Login', "Orkut"]); });
-  $("#services .google_btn").                 submit(function(){ _gaq.push(['_trackEvent', 'Login', "Google"]); });
-  $("#MR_login_link").                        submit(function(){ _gaq.push(['_trackEvent', 'Login', "Meu Rio"]); });
+  $("form#new_petition_signature_with_facebook"). submit(function(){ _gaq.push(['_trackEvent', 'Assinatura', "Assinar Via Facebook"]); });
+  $(".share_with_friends form").                  submit(function(){ _gaq.push(['_trackEvent', 'Share', "Postar no Mural do Amigo"]); });
+  $("#services a.facebook_btn").                  click(function(){ _gaq.push(['_trackEvent', 'Login', "Facebook"]); });
+  $("#services a.orkut_btn").                     click(function(){ _gaq.push(['_trackEvent', 'Login', "Orkut"]); });
+  $("#services a.google_btn").                    click(function(){ _gaq.push(['_trackEvent', 'Login', "Google"]); });
+  $("a#MR_login_link").                           click(function(){ _gaq.push(['_trackEvent', 'Login', "Meu Rio"]); });
 });
