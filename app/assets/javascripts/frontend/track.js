1ceb7acfdcc3645fa8909c04f4b6ef8c29a7d3c8
Nícolas Iensen <nicolas@quavio.com.br>
2012-02-13 11:22:35 -0200

    Fix login track code

--- app/assets/javascripts/frontend/track.js
+++ app/assets/javascripts/frontend/track.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
   $("form#new_petition_signature_with_facebook").                     submit(function(){ _gaq.push(['_trackEvent', 'Assinatura', "Assinar Via Facebook"]); });
   $(".share_with_friends form").                                      submit(function(){ _gaq.push(['_trackEvent', 'Share', "Postar no Mural do Amigo"]); });
-  $("a[rel=facebox]").click(function(){$("#services a.facebook_btn"). click(function(){ _gaq.push(['_trackEvent', 'Login', "Facebook"]); });
-  $("a[rel=facebox]").click(function(){$("#services a.orkut_btn").    click(function(){ _gaq.push(['_trackEvent', 'Login', "Orkut"]); });
-  $("a[rel=facebox]").click(function(){$("#services a.google_btn").   click(function(){ _gaq.push(['_trackEvent', 'Login', "Google"]); });
-  $("a[rel=facebox]").click(function(){$("a#MR_login_link").          click(function(){ _gaq.push(['_trackEvent', 'Login', "Meu Rio"]); });
+  $("a[rel=facebox]").click(function(){$("#services a.facebook_btn"). click(function(){ _gaq.push(['_trackEvent', 'Login', "Facebook"]); }); });
+  $("a[rel=facebox]").click(function(){$("#services a.orkut_btn").    click(function(){ _gaq.push(['_trackEvent', 'Login', "Orkut"]); }); });
+  $("a[rel=facebox]").click(function(){$("#services a.google_btn").   click(function(){ _gaq.push(['_trackEvent', 'Login', "Google"]); }); });
+  $("a[rel=facebox]").click(function(){$("a#MR_login_link").          click(function(){ _gaq.push(['_trackEvent', 'Login', "Meu Rio"]); }); });
   $("#meu_rio_is a.prev").                                            click(function(){ _gaq.push(['_trackEvent', 'Home', "Ver o Membro Anterior"]); });
   $("#meu_rio_is a.next").                                            click(function(){ _gaq.push(['_trackEvent', 'Home', "Ver o Próximo Membro"]); });
   $("#team_update a").                                                click(function(){ _gaq.push(['_trackEvent', 'Home', "Ir para o Blog"]); });
