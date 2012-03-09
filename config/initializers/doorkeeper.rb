ac1d3c9cc667675c919bb044899daab414947c73
Nícolas Iensen <nicolas@quavio.com.br>
2012-03-09 13:30:14 -0300

    Protect oauth applications paths

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -14,16 +14,16 @@ Doorkeeper.configure do
   # If you want to restrict the access to the web interface for
   # adding oauth authorized applications you need to declare the
   # block below
-  # admin_authenticator do |routes|
-  #   # Put your admin authentication logic here.
-  #   # If you want to use named routes from your app you need
-  #   # to call them on routes object eg.
-  #   # routes.new_admin_session_path
-  #   Admin.find_by_id(session[:admin_id]) || redirect_to(routes.new_admin_session_path)
-  # end
+  admin_authenticator do |routes|
+    # Put your admin authentication logic here.
+    # If you want to use named routes from your app you need
+    # to call them on routes object eg.
+    # routes.new_admin_session_path
+    redirect_to routes.root_path
+  end
 
   # Access token expiration time (default 2 hours)
-  # access_token_expires_in 2.hours
+  access_token_expires_in 2.years
 
   # Issue access tokens with refresh token (disabled by default)
   # use_refresh_token
