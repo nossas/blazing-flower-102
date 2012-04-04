bd7f3d58987d250076404fb1b3392eb720aa7ce7
Diogo Biazus <diogob@gmail.com>
2012-04-04 17:55:31 -0300

    removed unused lines of code

--- app/controllers/member_sessions_controller.rb
+++ app/controllers/member_sessions_controller.rb
@@ -29,10 +29,6 @@ class MemberSessionsController < Devise::SessionsController
   end
 
   def facebook_logout
-    split_token = session[:fb_token].split("|")
-    fb_api_key = split_token[0]
-    fb_session_key = split_token[1]
     redirect_to destroy_member_session_path
-    # redirect_to "http://www.facebook.com/logout.php?api_key=#{fb_api_key}&session_key=#{fb_session_key}&confirm=1&next=#{destroy_member_session_url}";
   end
 end
