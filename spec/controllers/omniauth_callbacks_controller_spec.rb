3b1e052332aed98f45459b6819586e21502f2348
Nícolas Iensen <nicolas@engage.is>
2012-06-19 17:27:03 -0300

    Remove useless tests

--- spec/controllers/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -21,17 +21,6 @@ describe OmniauthCallbacksController do
       subject
     end
 
-    context "with wrong auth data" do
-      before{ controller.stub(:auth_data).and_return(FACEBOOK_INVALID_AUTH_DATA) }
-      it{ expect{subject}.to_not change{Member.count} }
-      it{ expect{subject}.to_not change{ProviderAuthorization.count} }
-      it{ should redirect_to(root_path) }
-      it "should set a flash notice" do
-        subject
-        flash[:notice].should == 'You were unable to login'
-      end
-    end
-
     context "with right auth data" do
       before{ controller.stub(:auth_data).and_return(FACEBOOK_VALID_AUTH_DATA) }
       it{ expect{subject}.to change{Member.count}.by(1) }
