9034f5647a2b30e78767bcd5e4826c42c1b48e4f
Nicolas Iensen <nicolas@engage.is>
2012-10-26 21:14:48 -0200

    Bug fix after sign a petition Facebook share buttons were not showing up

--- app/assets/javascripts/frontend/petitions.js
+++ app/assets/javascripts/frontend/petitions.js
@@ -101,19 +101,20 @@ MR.petitions = {
     }
   },
 
-  loadTAF: function(){
-    if(window.location.hash == '#compartilhe'){
+	loadTAF: function(){
+		if(window.location.hash == '#compartilhe'){
 			$('.take_action').load(window.location.pathname + '/share', function(){
-				(function(d, s, id) {
-					var js, fjs = d.getElementsByTagName(s)[0];
-					if (d.getElementById(id)) return;
-					js = d.createElement(s); js.id = id;
-					js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=177910022269006";
-					fjs.parentNode.insertBefore(js, fjs);
-				}(document, 'script', 'facebook-jssdk'));
+				d = document;
+				s = 'script';
+				id = 'facebook-jssdk';
+				var js, fjs = d.getElementsByTagName(s)[0];
+				if (d.getElementById(id)) return;
+				js = d.createElement(s); js.id = id;
+				js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=177910022269006";
+				fjs.parentNode.insertBefore(js, fjs);
 			});
 		}
-  },
+	},
 
   runThese: function(params){
     $('option', '#member_zona').removeAttr('selected');
