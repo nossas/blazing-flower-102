929601c100f0dd80914345414cd9193bef8df585
Nícolas Iensen <nicolas@engage.is>
2012-11-12 18:28:14 -0200

    DJ for letter mail send

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -4,7 +4,7 @@ class LettersController < InheritedResources::Base
   def create
     create! do |success, failure|
       success.html do
-        LetterMailer.new_letter(@letter).delay.deliver
+        LetterMailer.new_letter(@letter).deliver
         redirect_to issue_letter_path(@letter.issue), :notice => "Valeu! Sua mensagem foi enviada!"
       end
     end
